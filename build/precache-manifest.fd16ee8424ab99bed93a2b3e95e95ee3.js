9bab02ab588a0d0c8c2bbcfdb5ed13b5d81fb158
Ville Lehtonen <violehtonen@gmail.com>
2020-07-02 09:50:40 +0200

    combine frontend to backend

---
+++ build/precache-manifest.fd16ee8424ab99bed93a2b3e95e95ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54331d04d9bbacbdcc9ddeef22212dbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e9c6c79705afb38458c",
+    "url": "/static/css/main.5e8f7e27.chunk.css"
+  },
+  {
+    "revision": "dbcb667f1f15efa52146",
+    "url": "/static/js/2.6f75bc6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6f75bc6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9c6c79705afb38458c",
+    "url": "/static/js/main.f464bfef.chunk.js"
+  },
+  {
+    "revision": "9605c4620dec2e8502b3",
+    "url": "/static/js/runtime-main.e4e63ad3.js"
+  }
+]);
