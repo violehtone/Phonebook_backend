3ead39a7bea59ad952c0779b7c77a558fc47bfb2
Ville Lehtonen <violehtonen@gmail.com>
2020-07-03 10:42:28 +0200

    exercise 3.22 - lint configuration

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length <3) {
-    console.log("Please provide the password as an argument: node mongo.js <password>")
-    process.exit(1)
+  console.log("Please provide the password as an argument: node mongo.js <password>")
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,11 +11,11 @@ const number = process.argv[4]
 
 const url = `mongodb+srv://fullstack:${password}@violehtonen.ovjrk.mongodb.net/test?retryWrites=true&w=majority`
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -27,20 +27,20 @@ const Person = mongoose.model('Person', personSchema)
 //})
 
 if(process.argv.length < 4) {
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(person)
-        })
-        mongoose.connection.close()
-    })    
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
+    mongoose.connection.close()
+  })
 }
 
 const person = new Person({
-    name: name,
-    number: number,
+  name: name,
+  number: number,
 })
 
 person.save().then(result => {
-    console.log(`Added ${name} number ${number} to phonebook`)
-    mongoose.connection.close()
+  console.log(`Added ${name} number ${number} to phonebook`)
+  mongoose.connection.close()
 })
